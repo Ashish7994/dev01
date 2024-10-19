0d0c3240b2780c1a58b29d2d353cf392e113cb45
Ashish7994 <ashish.admn@gmail.com>
2024-10-19 18:12:49 +0530

    dec2bin conversion

--- binary.cpp
+++ binary.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+using namespace std;
+
+int binary(int num)
+{
+    int ans = 0, pow = 1;
+    while(num > 0)
+    {
+        int rem = num % 2;
+        num = num / 2;
+        ans += pow * rem;
+        pow *= 10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int num = 32;
+    cout << "num: " << binary(num) << endl;
+    return 0;
+}
+
